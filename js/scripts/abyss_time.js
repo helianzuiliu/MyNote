d57a091cf13c194e7d688d22f70b57043d1447b6
赫连醉柳 <1273103690@qq.com>
2025-06-04 19:37:00 +0800

    vault backup: 2025-06-04 19:36:59
    受影响的文件:
    .obsidian/workspace.json
    01.Game/01.崩坏三/深渊.md
    js/scripts/abyss_time.js
    js/scripts/utils.js

--- js/scripts/abyss_time.js
+++ js/scripts/abyss_time.js
@@ -1,8 +1,12 @@
-let dt = require("luxon").DateTime
+// let dt = require("luxon").DateTime
+
+let dt = dv.luxon.DateTime
 
 let name = input.name
 let start_time = dt.fromObject(input.start_time)
 let finish_time = dt.fromObject(input.finish_time)
+
+let show_text = ""
 
 let now = dt.now()
 
@@ -10,22 +14,22 @@ if (start_time < now && now < finish_time) {
     // 深渊开了
     let diff_time = finish_time.diff(now, ["days", "hours", "minutes"]).toObject()
     if (diff_time.days != 0) {
-        show_text = "距离深渊结算还有 **" + diff_time.days + "** 天,还不用着急"
+        show_text = "距离 **"+name+"** 结算还有 *" + diff_time.days + "* 天,还不用着急"
     } else {
-        show_text = "距离深渊结算还有** "
-            + diff_time.hours + "** 小时 ** "
-            + parseInt(diff_time.minutes + 1) + " ** 分,今天你打深渊了吗"
+        show_text = "距离 **" + name +"** 结算还有* "
+            + diff_time.hours + "* 小时 *"
+            + parseInt(diff_time.minutes + 1) + "* 分,今天你打深渊了吗"
     }
 } else {
     // 深渊没开
     let diff_time = start_time.diff(now, ["days", "hours", "minutes"]).toObject()
     if (diff_time.days != 0) {
-        show_text = "深渊还没开,下一期深渊将会在 **" + diff_time.days + "** 天后开启"
+        show_text = "**"+name+"** 还没开,下一期深渊将会在 *" + diff_time.days + "* 天后开启"
     } else {
-        show_text = "深渊还没开,下一期深渊将会在** "
-            + diff_time.hours + "** 小时 ** "
+        show_text = "**" + name +"** 还没开,下一期深渊将会在* "
+            + diff_time.hours + "* 小时 * "
             + parseInt(diff_time.minutes + 1) + " ** 分钟后开启"
     }
 }
-console.log("Current timestamp:", now);
-// dv.span(arg1)
+
+dv.paragraph(show_text)
--- js/scripts/utils.js
+++
@@ -1,9 +0,0 @@
-
-function get(start_time, end_time) {
-    let dt= DataView.luxon.Datetime;
-}
-
-
-export{
-    get
-}
