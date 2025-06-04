f99ae65a871d3b96fb15c1986a50064a7820e6aa
赫连醉柳 <1273103690@qq.com>
2025-06-04 18:36:56 +0800

    vault backup: 2025-06-04 18:36:56
    受影响的文件:
    .obsidian/workspace.json
    01.Game/01.崩坏三/深渊.md
    01.Game/03.崩铁/深渊.md
    js/scripts/abyss_time.js

--- js/scripts/abyss_time.js
+++ js/scripts/abyss_time.js
@@ -1,19 +1,31 @@
-let a = {
-    "bh3": [
-        {
-            "name": "超弦空间",
-            "start_time": { weekday: 1, hour: 15 },
-            "end_time": { weekday: 3, hour: 22 },
-        }
-    ],
-    "genshin": [
-        {
-            "name": "深境螺旋",
-            "start_time": { weekday: 1, hour: 4 },
-            "end_time": { weekday: 15, hour: 3 },
-        }
-    ],
+let dt = require("luxon").DateTime
+
+let name = input.name
+let start_time = dt.fromObject(input.start_time)
+let finish_time = dt.fromObject(input.finish_time)
+
+let now = dt.now()
+
+if (start_time < now && now < finish_time) {
+    // 深渊开了
+    let diff_time = finish_time.diff(now, ["days", "hours", "minutes"]).toObject()
+    if (diff_time.days != 0) {
+        show_text = "距离深渊结算还有 **" + diff_time.days + "** 天,还不用着急"
+    } else {
+        show_text = "距离深渊结算还有** "
+            + diff_time.hours + "** 小时 ** "
+            + parseInt(diff_time.minutes + 1) + " ** 分,今天你打深渊了吗"
+    }
+} else {
+    // 深渊没开
+    let diff_time = start_time.diff(now, ["days", "hours", "minutes"]).toObject()
+    if (diff_time.days != 0) {
+        show_text = "深渊还没开,下一期深渊将会在 **" + diff_time.days + "** 天后开启"
+    } else {
+        show_text = "深渊还没开,下一期深渊将会在** "
+            + diff_time.hours + "** 小时 ** "
+            + parseInt(diff_time.minutes + 1) + " ** 分钟后开启"
+    }
 }
-
-
-console.log(a["bh3"]);
+console.log("Current timestamp:", now);
+// dv.span(arg1)
