e69d9234dd4fd3243e54ab1c3120ac0319c2a580
赫连醉柳 <1273103690@qq.com>
2025-06-05 22:08:57 +0800

    vault backup: 2025-06-05 22:08:56
    受影响的文件:
    .obsidian/workspace.json
    01.Game/01.崩坏三/深渊.md
    01.Game/02.原神/深渊.md
    Dashboard.md
    js/scripts/abyss_time.js
    js/scripts/abyss_time_bh3.js

--- js/scripts/abyss_time_bh3.js
+++ js/scripts/abyss_time_bh3.js
@@ -6,6 +6,7 @@ let now = dt.now()
 let name = input.name
 let start_time = input.start_time
 let finish_time = input.finish_time.minus({ seconds: 1 })
+let is_bh3 = input.is_bh3
 let diff_time
 
 let show_text = ""
@@ -15,7 +16,7 @@ if (start_time < now && now < finish_time) {
     diff_time = finish_time.diff(now, ["days", "hours", "minutes"])
     if (!now.hasSame(finish_time, "day")) {
         show_text = "距离 **" + name + "** 结算还有 *"
-            + (finish_time.weekday - now.weekday) + "* 天\n\n"
+            + (is_bh3 ? (finish_time.weekday - now.weekday) : (finish_time.day - now.day)) + "* 天\n\n"
         + "还不用着急"
     } else {
         show_text = "距离 **" + name + "** 结算还有* "
@@ -27,7 +28,7 @@ if (start_time < now && now < finish_time) {
     diff_time = start_time.diff(now, ["days", "hours", "minutes"])
     if (!now.hasSame(start_time, "day")) {
         show_text = "**" + name + "** 还没开,下一期** " + name + " **将会在 *"
-            +  (start_time.weekday - now.weekday) + "* 天后开启\n\n"
+            + (is_bh3 ? (start_time.weekday - now.weekday) : (finish_time.day - now.day)) + "* 天后开启\n\n"
             + "去看看其他的吧"
     } else {
         show_text = "**" + name + "** 还没开,下一期** " + name + " **将会在* "
