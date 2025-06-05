12f86ba105cadf6c356eb3699ec934752242011a
赫连醉柳 <1273103690@qq.com>
2025-06-05 20:08:53 +0800

    vault backup: 2025-06-05 20:08:52
    受影响的文件:
    01.Game/01.崩坏三/深渊.md
    js/scripts/abyss_time.js

--- js/scripts/abyss_time.js
+++ js/scripts/abyss_time.js
@@ -3,16 +3,17 @@
 let dt = dv.luxon.DateTime
 
 let name = input.name
-let start_time = dt.fromObject(input.start_time)
-let finish_time = dt.fromObject(input.finish_time)
+let start_time = input.start_time
+let finish_time = input.finish_time
 
 let show_text = ""
+let diff_time
 
 let now = dt.now()
 
 if (start_time < now && now < finish_time) {
     // 深渊开了
-    let diff_time = finish_time.diff(now, ["days", "hours", "minutes"]).toObject()
+    diff_time = finish_time.diff(now, ["days", "hours", "minutes"]).toObject()
     if (diff_time.days != 0) {
         show_text = "距离 **"+name+"** 结算还有 *" + diff_time.days + "* 天,还不用着急"
     } else {
@@ -22,15 +23,16 @@ if (start_time < now && now < finish_time) {
     }
 } else {
     // 深渊没开
-    let diff_time = start_time.diff(now, ["days", "hours", "minutes"]).toObject()
+    diff_time = start_time.diff(now, ["days", "hours", "minutes"]).toObject()
     if (diff_time.days != 0) {
         show_text = "**"+name+"** 还没开,下一期深渊将会在 *" + diff_time.days + "* 天后开启"
     } else {
         show_text = "**" + name +"** 还没开,下一期深渊将会在* "
             + diff_time.hours + "* 小时 * "
-            + parseInt(diff_time.minutes + 1) + " ** 分钟后开启"
+            + parseInt(diff_time.minutes + 1) + " * 分钟后开启"
     }
 }
 
+// dv.span(diff_time)
 dv.header(1, name)
 dv.paragraph(show_text)
