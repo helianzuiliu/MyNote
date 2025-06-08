81eb114836f66d4cd46335b1a31443f1c81b5a9d
赫连醉柳 <1273103690@qq.com>
2025-06-08 14:31:57 +0800

    vault backup: 2025-06-08 14:31:57
    受影响的文件:
    js/scripts/abyss_time.js

--- js/scripts/abyss_time.js
+++ js/scripts/abyss_time.js
@@ -1,6 +1,6 @@
 // let dt = require("luxon").DateTime
 
-const { GameTimeResolver } = customJS
+let { GameTimeResolver } = customJS
 let dt = dv.luxon.DateTime
 let now = dt.now()
 let name = input.name
@@ -10,5 +10,4 @@ let show_text = "123"
 dv.span(GameTimeResolver.getNextSettlementTime(dv,"bh3"))
 
 dv.header(1, name)
-// // dv.span(diff_time)
 dv.paragraph(show_text)
