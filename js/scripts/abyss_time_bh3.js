d49a7600ba8d9b705422c204d3c05daa821b73d4
赫连醉柳 <1273103690@qq.com>
2025-06-09 10:19:02 +0800

    vault backup: 2025-06-09 10:19:01
    受影响的文件:
    .obsidian/workspace.json
    01.Game/01.崩坏三/深渊.md
    js/scripts/abyss_time_bh3.js

--- js/scripts/abyss_time_bh3.js
+++ js/scripts/abyss_time_bh3.js
@@ -10,32 +10,31 @@ let diff_time
 
 let show_text = ""
 
+
+
 if (start_time < now && now < finish_time) {
     // 深渊开了
     diff_time = finish_time.diff(now, ["days", "hours", "minutes"])
     if (!now.hasSame(finish_time, "day")) {
-        show_text = "距离 **" + name + "** 结算还有 *"
-            + (finish_time.weekday - now.weekday) + "* 天\n\n"
-        + "还不用着急"
+        show_text = `距离 **${name}** 结算还有 *${finish_time.weekday - finish_time.weekday}* 天还不用着急`
     } else {
-        show_text = "距离 **" + name + "** 结算还有* "
-            + diff_time.hours + "* 小时 *" + parseInt(diff_time.minutes + 1) + "* 分\n\n"
-            + "你使用了雄伟药水,你感觉到身体,啊啊啊啊啊..."
+        show_text = `距离 **${name}** 结算还有`
+            + ` *${diff_time.hours}* 小时 *${parseInt(diff_time.minutes + 1)}* 分`
+            + `\n\n你使用了雄伟药水,你感觉到身体,啊啊啊啊啊...`
     }
 } else {
     // 深渊没开
     diff_time = start_time.diff(now, ["days", "hours", "minutes"])
     if (!now.hasSame(start_time, "day")) {
-        show_text = "**" + name + "** 还没开,下一期** " + name + " **将会在 *"
-            + (start_time.weekday - now.weekday)  + "* 天后开启\n\n"
-            + "去看看其他的吧"
+        show_text = `**${name}** 还没开,下一期** ${name} **将会在* ${start_time.weekday - now.weekday} *天后开启`
+            + `\n\n去看看其他的吧`
     } else {
         show_text = "**" + name + "** 还没开,下一期** " + name + " **将会在* "
-            + diff_time.hours + "* 小时 * " + parseInt(diff_time.minutes + 1) + " * 分钟后开启\n\n"
-            + "去看看其他的吧"
+            + diff_time.hours + "* 小时 * " + parseInt(diff_time.minutes + 1) + " * 分钟后开启"
+            + "\n\n去看看其他的吧"
     }
 }
 
-dv.header(1, name)
+dv.header(2, name)
 // dv.span(diff_time)
 dv.paragraph(show_text)
