17fe5a4309e9eee06ca0a71971fcfe3a52fd8468
赫连醉柳 <1273103690@qq.com>
2025-06-22 17:26:38 +0800

    vault backup: 2025-06-22 17:26:38
    受影响的文件:
    .obsidian/workspace.json
    js/scripts/game/game_data.js
    js/scripts/utils/time_util.js

--- js/scripts/game/game_data.js
+++ js/scripts/game/game_data.js
@@ -5,10 +5,12 @@ class GameTimeResolver {
         switch (name) {
             case "崩坏三":
                 return this.getNextSettlementTime_BH3(name, dt)
-
             case "原神":
                 return this.getNextSettlementTime_GENSHIN(name, dt)
-
+            case "崩坏：星穹铁道":
+                return this.getNextSettlementTime_BHSR(name, dt)
+            case "鸣嘲":
+                return this.getNextSettlementTime_MINGCHAO(name, dt)
             default:
                 return ("Unknown game name:" + name)
         }
@@ -20,13 +22,29 @@ class GameTimeResolver {
         const now = dt.now()
         const base_time = now.set({ hour: 4, minute: 0, second: 0, millisecond: 0 })
 
+        let start_time_1 = base_time.set({ weekday: 1, hour: 15 })
+        let finish_time_1 = base_time.set({ weekday: 3, hour: 22 })
+        let start_time_2 = base_time.set({ weekday: 5, hour: 15 })
+        let finish_time_2 = base_time.set({ weekday: 7, hour: 22 })
+
+        let a = finish_time_1 < now && now < finish_time_2 ? {
+            start_time: start_time_2,
+            finish_time: finish_time_2,
+        } : {
+            start_time: start_time_1,
+            finish_time: finish_time_1,
+        }
+
         let obj = [{
             name: "超弦空间",
-            start_time: base_time.set({ hour: 15, weekday: 1 }),
-            finish_time: base_time.set({ hour: 22, weekday: 3 }),
+            ...a
         }, {
             name: "记忆战场",
             start_time: base_time.set({ weekday: 2 }),
+            finish_time: base_time.set({ weekday: 1 }).plus({ weeks: 1 }),
+        }, {
+            name: "往世乐土",
+            start_time: base_time.set({ weekday: 1 }),
             finish_time: base_time.set({ weekday: 1 }).plus({ weeks: 1 }),
         }]
 
@@ -39,7 +57,6 @@ class GameTimeResolver {
     }
 
     getNextSettlementTime_GENSHIN(name, dt) {
-        // let d = data["genshin"][name]
         const { time_util } = customJS
 
         const now = dt.now()
@@ -57,10 +74,20 @@ class GameTimeResolver {
 
         return obj.map(b => ({
             ...b,
-            // 剩余时间
             remaining_time: time_util.diffTime(now, b.finish_time),
             from: "原神",
         }))
+    }
+
+    getNextSettlementTime_BHSR(name, dt) {
+        const { time_util } = customJS
+
+        const now = dt.now()
+        const base_time = now.set({ hour: 4, minute: 0, second: 0, millisecond: 0 })
+
+    }
+
+    getNextSettlementTime_MINGCHAO(name, dt) {
 
     }
 }
--- js/scripts/utils/time_util.js
+++ js/scripts/utils/time_util.js
@@ -1,21 +1,6 @@
 // let dt=require('luxon').DateTime
 class time_util {
 
-    /**
-     * 获取下个月的第一天
-     * @param {*} time ISO时间字符串，获取指定时间的下个月第一天
-     * @param {*} dt 
-     * @returns 
-     */
-    getFirstDateInNextMonth(time, dt) {
-        // 获取当前月份的天数
-        const t = dt.fromISO(time);
-        // t.set({ day: 1 })
-        t.plus({ months: 1 });
-
-        return t;
-    }
-
     diffTime(start_time, finish_time) {
         let duration = finish_time.diff(start_time, ["days", "hours", "minutes"])
         if (duration.get("days") == 0) {
